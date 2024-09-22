9f1e5e0cacf63cfc498b551826acbd78963b0b64
Emrecanbl <123264067+Emrecanbl@users.noreply.github.com>
2024-09-22 18:42:38 +0300

    Delete build directory

--- build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "xiao_ble/nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/ncs/bt-fund-main/lesson1/blefund_less1_exer1;c:/ncs/ble_beacon" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
